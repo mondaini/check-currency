97d5c99d4f0fb4d143d8518844f1341a94314524
Filipe Mondaini <filipe@mondaini.me>
2020-08-14 19:04:10 -0300

    wip

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
+from os import getenv
 from flask import Flask
+from flask import request
+from requests import get
+
+from slackeventsapi import SlackEventAdapter
+from slack import WebClient
+
+
+EXCHANGE_API_KEY = getenv('EXCHANGE_API_KEY')
+SLACK_SIGNING_SECRET = getenv('SLACK_SIGNING_SECRET')
+SLACK_BOT_TOKEN = getenv("SLACK_BOT_TOKEN")
+
 app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
+slack_client = WebClient(SLACK_BOT_TOKEN)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/quote', methods=['GET', 'POST'])
+def quote():
+    url = "https://openexchangerates.org/api/latest.json?app_id={}&symbols=BRL&prettyprint=true".format(EXCHANGE_API_KEY)
+
+    response = get(url)
+
+    return response.json()
+
+
+@slack_events_adapter.on("message")
+@app.route('/message', methods=['POST'])
+def mention(event_data):
+    data = quote()
+    req_body = request.body
+
+    data.get("rates").get("BRL")
+    message = event_data["event"]
+
+    if message.get("subtype") is None and "Dólar" in message.get('text'):
+        channel = message["channel"]
+        message = ":money: %s" % data.get("rates").get("BRL")
+        slack_client.chat_postMessage(channel=channel, text=message)
