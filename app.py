755178afbc7687cf38074d518d77b7b450271a5f
Filipe Mondaini <filipe@mondaini.me>
2020-08-14 21:03:02 -0300

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import request
 from requests import get
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
+from decimal import Decimal
 
 EXCHANGE_API_KEY = getenv('EXCHANGE_API_KEY')
 SLACK_SIGNING_SECRET = getenv('SLACK_SIGNING_SECRET')
@@ -34,9 +35,11 @@ def mention(event_data):
     data = quote()
     data.get("rates").get("BRL")
     message = event_data["event"]
-    rate = data.get("rates").get("BRL")
 
-    if message.get("subtype") is None and "Dólar" in message.get('text'):
+    rate = Decimal(data.get("rates").get("BRL"))
+    husky = rate * Decimal(0.975)
+
+    if message.get("subtype") is None and "dólar" in message.get('text'):
         channel = message["channel"]
-        message = ":money_with_wings: %s" % rate
+        message = ":money_with_wings::money_with_wings::money_with_wings:\nR$ {:.2f}\n:husky: {:.2f} ".format(rate, husky)
         slack_client.chat_postMessage(channel=channel, text=message)
