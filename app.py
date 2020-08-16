3e1170d893e063175d3812feffd41fb48f428378
Filipe Mondaini <filipe@mondaini.me>
2020-08-16 13:30:57 -0300

    fix

--- app.py
+++ app.py
@@ -9,22 +9,28 @@ from decimal import Decimal
 EXCHANGE_API_KEY = getenv('EXCHANGE_API_KEY')
 SLACK_SIGNING_SECRET = getenv('SLACK_SIGNING_SECRET')
 SLACK_BOT_TOKEN = getenv("SLACK_BOT_TOKEN")
-
-app = Flask(__name__)
-slack_client = WebClient(SLACK_BOT_TOKEN)
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
-
 MESSAGE = """:dollar::dollar::dollar:
 
 :flag-br: = {:.2f}
 :husky: = {:.2f} (2,5%)
 """
 
+app = Flask(__name__)
+slack_client = WebClient(SLACK_BOT_TOKEN)
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
+
+
+def quote():
+    url = "https://openexchangerates.org/api/latest.json?app_id={}&symbols=BRL&prettyprint=true".format(EXCHANGE_API_KEY)
+
+    response = get(url)
+
+    return response.json()
+
 
 @slack_events_adapter.on("message")
 def mention(event_data):
     message = event_data["event"]
-
 
     if message.get("subtype") is None and "dólar" in message.get('text'):
         data = quote()
